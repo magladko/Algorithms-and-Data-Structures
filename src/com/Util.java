ce287849872d6b98f051e4f9610000ef45e7edaa
magladko <kornel.marek@gmail.com>
2022-02-06 17:55:24 +0100

    hoare FIX

--- src/com/Util.java
+++ src/com/Util.java
@@ -8,6 +8,10 @@ public class Util {
 //        alg1(10);
     }
 
+    public enum Podzial {
+        PARTITION, SPLIT
+    }
+
     public static void swapInt(int[] arr, int i1, int i2) {
         int temp = arr[i1];
         arr[i1] = arr[i2]; arr[i2] = temp;
